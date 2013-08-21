1e539e7a9e19d860fa16cddfa38fb06ec52d6fec
Jack White <>
2013-08-21 09:20:35 -0700

    Initial commit
    Added metadata for the userscript file

---
+++ fakku_onepage.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name            Fakku OnePage
+// @author          hrroon
+// @version         0.1
+// @date            2013-08-20
+// @namespace       http://userscripts.org/users/529104
+// @description     Shows all pages at once in online view and allows keyboard navigation
+// @include         http://www.fakku.net/doujinshi/*/read*
+// @include         http://www.fakku.net/manga/*/read*
+// @require         http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js
+// ==/UserScript==
